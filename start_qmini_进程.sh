12cee9f3ad03429b1518a03ab9a5243a711d852e
bird <liyp9503@qq.com>
2025-12-08 16:22:11 +0800

    export修正，重新调零

--- start_qmini_进程.sh
+++ start_qmini_进程.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:install/x86_64/lib/
+# 将项目库路径放在最前面，确保优先使用项目自己编译的库
+# 这样可以避免链接到ROS或其他系统的yaml-cpp库（版本可能不兼容）
+export LD_LIBRARY_PATH=$(pwd)/install/x86_64/lib/:$LD_LIBRARY_PATH
+
+# 运行程序
 ./install/x86_64/bin/run_interface &> qmini_log.log &
 
