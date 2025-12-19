c988410f6f456af2168db53330624c4931e8868c
qmini <qmini@localhost>
2025-12-19 16:48:43 +0800

    除了手柄自转问题，imu都搞定了

--- imu_receiver.py
+++ imu_receiver.py
@@ -224,4 +224,4 @@ def read_imu_data(port="/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Br
 
 
     return result
-read_imu_data(port="/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0003-if00-port0", baudrate=921600, timeout=1)
+# read_imu_data(port="/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0003-if00-port0", baudrate=921600, timeout=1)
